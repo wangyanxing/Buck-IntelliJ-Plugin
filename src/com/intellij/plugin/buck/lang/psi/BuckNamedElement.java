131fcf4a3c7a9370a4269d3fe307234a4c35a514
wangyanxing <demiwangya@gmail.com>
2015-08-02 02:54:27 -0700

    Removed unused files

--- src/com/intellij/plugin/buck/lang/psi/BuckNamedElement.java
+++
@@ -1,6 +0,0 @@
-package com.intellij.plugin.buck.lang.psi;
-
-import com.intellij.psi.PsiNameIdentifierOwner;
-
-public interface BuckNamedElement extends PsiNameIdentifierOwner {
-}
--- src/com/intellij/plugin/buck/lang/psi/impl/BuckNamedElementImpl.java
+++ src/com/intellij/plugin/buck/lang/psi/impl/BuckNamedElementImpl.java
@@ -2,11 +2,11 @@ package com.intellij.plugin.buck.lang.psi.impl;
 
 import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import com.intellij.lang.ASTNode;
-import com.intellij.plugin.buck.lang.psi.BuckNamedElement;
+import com.intellij.psi.PsiNameIdentifierOwner;
 import org.jetbrains.annotations.NotNull;
 
 public abstract class BuckNamedElementImpl
-    extends ASTWrapperPsiElement implements BuckNamedElement {
+    extends ASTWrapperPsiElement implements PsiNameIdentifierOwner {
 
   public BuckNamedElementImpl(@NotNull ASTNode node) {
     super(node);
