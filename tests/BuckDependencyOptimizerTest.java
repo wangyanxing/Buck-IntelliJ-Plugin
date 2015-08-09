31e7209c54b259704251b54d0dcc16ef52ab6998
longma <lm675@cornell.edu>
2015-08-08 22:24:21 -0700

    dependency optimizer test cases

---
+++ tests/BuckDependencyOptimizerTest.java
@@ -0,0 +1,18 @@
+import com.intellij.codeInsight.actions.OptimizeImportsAction;
+import com.intellij.ide.DataManager;
+
+public class BuckDependencyOptimizerTest extends BuckTestCase {
+  public void testSimple() {
+    doTest();
+  }
+
+  public void testLintRule() {
+    doTest();
+  }
+
+  private void doTest() {
+    myFixture.configureByFile("dependencyOptimizer/" + getTestName(false) + "/BUCK");
+    OptimizeImportsAction.actionPerformedImpl(DataManager.getInstance().getDataContext(myFixture.getEditor().getContentComponent()));
+    myFixture.checkResultByFile("dependencyOptimizer/" + getTestName(true) + "/after.BUCK");
+  }
+}
